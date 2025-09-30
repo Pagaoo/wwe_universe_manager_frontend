e7209498dfb7526d925bd560648e2db1ca0a7a55
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-09-30 17:49:32 -0300

    feat: a little bit of style to the wrestler list

--- src/app/components/wrestler-list/wrestler-list.ts
+++ src/app/components/wrestler-list/wrestler-list.ts
@@ -3,17 +3,22 @@ import { Wrestler } from '../../models/wrestler';
 import { WrestlerService } from '../../services/wrestler';
 import { MatDialog } from '@angular/material/dialog';
 import { WrestlerEditDialog } from '../wrestler-edit-dialog/wrestler-edit-dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-wrestler-list',
   standalone: true,
   templateUrl: './wrestler-list.html',
   styleUrl: './wrestler-list.css',
-  imports: []
+  imports: [MatTableModule, MatButtonModule, MatIconModule]
 })
 export class WrestlerListComponent implements OnInit{
   wrestlers: Wrestler[] = []
   isLoading = true
+
+  displayedColumns: string[] = ["name", "weight", "height", "age", "actions"]
 
   constructor(private wrestlerService: WrestlerService, public dialog: MatDialog) {}
 
@@ -37,13 +42,7 @@ export class WrestlerListComponent implements OnInit{
 
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
-        this.wrestlerService.saveEditWrestler(result.id, wrestler, result).subscribe({
-          next: (res) => {
-            const index = this.wrestlers.findIndex(wrestler => wrestler.id === res.id)
-            if(index !== -1) this.wrestlers[index] = res
-          },
-          error: (error) => console.log(error)
-        })
+        this.loadWrestlers()
       }
     })
   }
