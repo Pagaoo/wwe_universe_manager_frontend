9367a04fb79e44eb64a93b3d19f08597be894e77
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-09-30 17:49:52 -0300

    fix: fixed not realoading the uptaded information into to the wreslers list

--- src/app/services/wrestler.ts
+++ src/app/services/wrestler.ts
@@ -5,8 +5,6 @@ import { Wrestler } from '../models/wrestler';
 import { environment } from '../../environments/environment';
 import { buildJsonPatch } from '../utils/buildJsonPatch';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +17,7 @@ export class WrestlerService {
     return this.http.get<Wrestler[]>(this.apiUrl)
   }
 
-  saveEditWrestler(id: number, original: Wrestler, updated: Wrestler): Observable<any> {
+  updateWrestler(id: number, original: Wrestler, updated: Wrestler): Observable<any> {
     const patchBody = buildJsonPatch(original, updated)
     return this.http.patch(`${this.apiUrl}/${id}`, patchBody)
   }
