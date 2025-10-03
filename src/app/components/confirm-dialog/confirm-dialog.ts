653b02b1c535704d5937fe2739184710ca37bea2
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-10-03 09:11:01 -0300

    feat: added delete api endpoint to delete wrestler

---
+++ src/app/components/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.css',
+})
+export class ConfirmDialog {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+  onConfirmClick(): void {
+    this.dialogRef.close(true);
+  }
+}
