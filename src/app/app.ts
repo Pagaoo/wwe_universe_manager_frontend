cf71dc3e99551aa1d8a2e22808ec4c3e4c59b516
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-10-09 11:14:25 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './layout/sidebar/sidebar';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/wrestler-list/wrestler-list.ts
+++ src/app/components/wrestler-list/wrestler-list.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Wrestler } from '../../models/wrestler';
 import { WrestlerService } from '../../services/wrestler';
 import { MatDialog } from '@angular/material/dialog';
